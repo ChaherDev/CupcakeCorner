d0f7d9dba776fd4b0c52ffe254c43e0280b90dff
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-10-09 19:12:00 +0200

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Chaher Machhour on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
